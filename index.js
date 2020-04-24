bd9d3e904ab6fdeef0b9a89659e6d9ae252c8b71
lilas-saleh <lilas.ahmad.saleh@gmail.com>
2020-04-24 13:30:45 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+// 1st 
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply);
+ 
+// 2nd
+let random = Math.floor(Math.random()*10)+1;
+console.log(random);
+
+// 3rd 
+let mod = 10%6;
+console.log(mod);
+
+// 4th
+let max = Math.max (20,1,6,5,0);
+console.log(max);
